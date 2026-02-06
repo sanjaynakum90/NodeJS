2e38b8253c5f34d955d29436db661ecd3c047e1f
sanjaynakum90 <sanjaynakum4545@gmail.com>
2026-02-06 16:50:22 +0530

    update

---
+++ 06-Express-CRUD/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res(200).json("hello from server")
+})
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log("server is listing on port");
+
+})
